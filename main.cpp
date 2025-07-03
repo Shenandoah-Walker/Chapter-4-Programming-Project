ca954f1a6687f39439b398c5535e6a06c5eae280
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-07-03 16:48:44 +0000

    Created Shipping Charges Program

--- main.cpp
+++ main.cpp
@@ -1,105 +1,66 @@
-//Geometry Calculator Program
+//Shipping Charges Program
 #include <iostream>
 #include <iomanip>
-#include <string>
+#include <cmath>
 
 using namespace std;
 int main() {
-
   //Initialize variables
-  double choice, area, base, height, radius, length, width;
+  int weight, distance;
+  double shippingCharge;
 
-  //Set variables to 0 in case they are not used
-  area = 0;
-  choice = 0;
-  base = 0;
-  height = 0;
-  radius = 0;
-  length = 0;
-  width = 0;
+  //Create a header for the program
+  cout << "Shipping Charge Calculator for Fast Freight Shipping Company" << endl;
+  cout << endl;
 
-  //Display menu and get request user choice
-  cout << "Geometry Calculator" << endl;
-  cout <<  "1. Calculate the Area of a Circle" << endl;
-  cout << "2. Calculate the Area of a Rectangle" << endl;
-  cout << "3. Calculate the Area of a Triangle" << endl;
-  cout << "4. Quit" << endl;
-  cout << "Enter your choice (1-4): " << flush;
-  cin >> choice;
-
-  //Use the fixed function to set the floating-point notation to fixed-point
-  cout << fixed;
+  //Prompt user to enter weight and ensure that the weight is in the correct range: between 0 and 20 kilograms
+  cout << "Enter the weight of the package (in kilograms):" << endl;
+  cin >> weight;
   
-  //For option 1, get radius and calculate area
-  if (choice == 1) {
-    cout << "Enter the radius of the circle: " << endl;
-    cin >> radius;
-    //Ensure radius is positive
-    if (radius >= 0) {
-    area = 3.14159 * radius * radius;
-    cout << "The area of the circle is " << area << endl;
+  if (weight <= 0 || weight > 20) {
+    cout << "Invalid weight. Weight must be greater than 0. Please restart the program." << endl;
     return 0;
-    }
-    //If radius is negative, display error message
-    else {
-      cout << "Error: Negative values are not allowed. Please restart the program." << endl;
-      return 0;
-    }
   }
-
-  //For option 2, get length and width and calculate area
-  else if (choice == 2) {
-    cout << "Enter the length of the rectangle: " << endl;
-    cin >> length;
-    cout << "Enter the width of the rectangle: " << endl;
-    cin >> width;
-    
-    //Ensure length and width are positive
-    if (length >= 0 && width >= 0) {
-    area = length * width;
-    cout << "The area of the rectangle is " << area << endl;
+  else if (weight > 20) {
+    cout << "Only packages weighing 20 kilograms or less can be shipped." << endl;
     return 0;
-    }
-    
-    //If length or width is negative, display error message
-    else {
-      cout << "Error: Negative values are not allowed. Please restart the program." << endl;
-      return 0;
-    }
   }
+  
+  //Prompt user to enter distance and ensure that the distance is in the correct range: between 10 and 3000 miles
+  cout << "Enter the distance to be shipped (in miles):" << endl;
+  cin >> distance;
 
-  //For option 3, get base and height and calculate area
-  else if (choice == 3) {
-    cout << "Enter the base of the triangle: " << endl;
-    cin >> base;
-    cout << "Enter the height of the triangle: " << endl;
-    cin >> height;
-
-    //Ensure base and height are positive
-    if (base >= 0 && height >= 0) {
-    area = base * height * 0.5;
-    cout << "The area of the triangle is " << area << endl;
+  if (distance < 10) {
+    cout << "Distance must be at least 10 miles." << endl;
     return 0;
-    }
-
-    //If base or height is negative, display error message
-    else {
-      cout << "Error: Negative values are not allowed. Please restart the program." << endl;
-      return 0;
-    }
   }
-
-  //For option 4, quit the program
-  else if (choice == 4) {
-    cout << "End of program" << endl;
+  else if (distance > 3000) {
+    cout << "Distance must be 3000 miles or less." << endl;
     return 0;
   }
 
-  //For any other option, display error message
+  //Calculate shipping charge
+  if (weight <= 2) {
+    shippingCharge = (distance % 500) * 1.10;
+    cout << "The shipping charge is $" << shippingCharge << endl;
+    return 0;
+  }
+  
+  else if (weight > 2 && weight <= 6) {
+    shippingCharge = (distance % 500) * 2.20;
+    cout << "The shipping charge is $" << shippingCharge << endl;
+    return 0;
+}
+  else if (weight > 6 && weight <= 10) {
+    shippingCharge = (distance % 500) * 3.70;
+    cout << "The shipping charge is $" << shippingCharge << endl;
+    return 0;
+  }
+
   else {
-    cout << "Error: Invalid choice. Please restart the program." << endl;
+    shippingCharge = (distance % 500) * 4.80;
+    cout << "The shipping charge is $" << shippingCharge << endl;
     return 0;
   }
-
-
+  
 }
