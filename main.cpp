b8260adc0932eba71086d3ef7e297ceb495230ad
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-07-03 17:09:16 +0000

    Updated shipping calculations

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 //Shipping Charges Program
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 
 using namespace std;
 int main() {
+  
   //Initialize variables
   int weight, distance;
   double shippingCharge;
+  
 
   //Create a header for the program
   cout << "Shipping Charge Calculator for Fast Freight Shipping Company" << endl;
   cout << endl;
+  
+
+  //Set precision for output
+  cout << fixed << setprecision(2);
+  
 
   //Prompt user to enter weight and ensure that the weight is in the correct range: between 0 and 20 kilograms
-  cout << "Enter the weight of the package (in kilograms):" << endl;
+  cout << "Enter the weight of the package (in kilograms): " << flush;
   cin >> weight;
   
   if (weight <= 0 || weight > 20) {
@@ -25,6 +31,7 @@ int main() {
     cout << "Only packages weighing 20 kilograms or less can be shipped." << endl;
     return 0;
   }
+
   
   //Prompt user to enter distance and ensure that the distance is in the correct range: between 10 and 3000 miles
   cout << "Enter the distance to be shipped (in miles):" << endl;
@@ -39,26 +46,39 @@ int main() {
     return 0;
   }
 
+  
   //Calculate shipping charge
   if (weight <= 2) {
-    shippingCharge = (distance % 500) * 1.10;
+    shippingCharge = (distance / 500) * 1.10;
+     if (shippingCharge == 0) {
+       shippingCharge = 1.10;
+     }
     cout << "The shipping charge is $" << shippingCharge << endl;
     return 0;
   }
   
   else if (weight > 2 && weight <= 6) {
-    shippingCharge = (distance % 500) * 2.20;
+    shippingCharge = (distance / 500) * 2.20;
+    if (shippingCharge == 0) {
+       shippingCharge = 2.20;
+     }
     cout << "The shipping charge is $" << shippingCharge << endl;
     return 0;
 }
   else if (weight > 6 && weight <= 10) {
-    shippingCharge = (distance % 500) * 3.70;
+    shippingCharge = (distance / 500) * 3.70;
+    if (shippingCharge == 0) {
+       shippingCharge = 3.70;
+     }
     cout << "The shipping charge is $" << shippingCharge << endl;
     return 0;
   }
 
   else {
-    shippingCharge = (distance % 500) * 4.80;
+    shippingCharge = (distance / 500) * 4.80;
+    if (shippingCharge == 0) {
+       shippingCharge = 4.80;
+     }
     cout << "The shipping charge is $" << shippingCharge << endl;
     return 0;
   }
